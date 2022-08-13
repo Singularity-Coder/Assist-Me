0e8078617d8ac4bb6b8cd79aab0c0f5a756c81c7
hithesh vurjana <hitheshvurjana@hitheshs-MacBook-Air.local>
2022-08-13 17:59:12 +0530

    first commit

---
+++ BlaBlaBla/app/src/main/java/com/singularitycoder/blablabla/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.singularitycoder.blablabla
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// https://beta.openai.com/examples
+// https://beta.openai.com/overview
+// https://beta.openai.com/docs/quickstart/build-your-application
+// https://developers.google.com/ml-kit/guides
+// https://github.com/tesseract-ocr/tesseract
+// https://developers.google.com/ml-kit
+// https://stackoverflow.com/questions/47471159/how-to-extract-text-from-an-audio-file
+// extract text from audio android programmatically
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
